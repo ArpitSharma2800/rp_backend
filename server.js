27afb9199e51ddd180dc751c9f37904a32c1deee
ArpitSharma2800 <1arpit2000sharma@gmail.com>
2021-07-22 21:59:22 +0530

    Update server.js
    update server port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 const cors = require("cors");
 app.use(cors());
-const PORT = 3500;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`server started at port : ${PORT}`);
